db5062b7b8724c28ecf59a2b03abd31b6726294b
nikolaskurs <nikolaskurs@gmail.com>
2025-10-12 03:20:42 +0100

    Initial Commit

---
+++ setup/version.js
@@ -0,0 +1,11 @@
+const log = require('./log.js')
+const clientVersion = "1.0.0" // Manually Set the Client Version here
+
+function alertNewVersion(globalVersion) {
+    if (clientVersion !== globalVersion)
+        log.warn(`Your client version (${clientVersion}) is out of date. Please update to ${globalVersion} by downloading the latest release.`)
+}
+
+module.exports = {
+    alertNewVersion
+}
